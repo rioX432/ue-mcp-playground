7e05a7d8e2517b9d38b2cb678d0b6ce970d9d83d
rioX432 <omooooori432@gmail.com>
2026-06-22 00:38:47 +0900

    B5: C++ AHUD score display + GameMode wiring (GameState+HUD), set default GameMode (#11)

---
+++ Source/MCPPlayground/Public/MCPPlaygroundGameMode.h
@@ -0,0 +1,21 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MCPPlaygroundGameMode.generated.h"
+
+/**
+ * Default game mode for the playground. Wires together the score GameState and the
+ * debug HUD so the collectible mechanic works in PIE out of the box. The default
+ * pawn is added with the character work (#9).
+ */
+UCLASS()
+class MCPPLAYGROUND_API AMCPPlaygroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMCPPlaygroundGameMode();
+};
